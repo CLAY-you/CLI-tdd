3c2c075ba68141afd7663d13c86c3c389db2d5f7
Hongkui Zhang <hongkui.zhang@thoughtworks.com>
2023-03-26 15:47:41 +0800

    feat: quick pass setting boolean option to true

--- src/main/java/com/geektime/tdd/cli/Args.java
+++ src/main/java/com/geektime/tdd/cli/Args.java
@@ -1,7 +1,14 @@
 package com.geektime.tdd.cli;
 
+import java.lang.reflect.Constructor;
+
 public class Args {
     public static <T> T parse(Class<T> optionType, String... args) {
-        return null;
+        Constructor<?> constructor = optionType.getDeclaredConstructors()[0];
+        try {
+            return (T) constructor.newInstance(true);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
