dad50706c6ce00ed3658e22f13b32c57d4df8423
Hongkui Zhang <hongkui.zhang@thoughtworks.com>
2023-03-26 19:44:39 +0800

    refactor: let str option parser extends int option parser to override the only difference code part parse value method

--- src/main/java/com/geektime/tdd/cli/IntOptionParser.java
+++ src/main/java/com/geektime/tdd/cli/IntOptionParser.java
@@ -6,6 +6,11 @@ class IntOptionParser implements OptionParser {
     @Override
     public Object parse(List<String> arguments, Option option) {
         int index = arguments.indexOf(String.format("-%s", option.value()));
-        return Integer.parseInt(arguments.get(index + 1));
+        String value = arguments.get(index + 1);
+        return parseValue(value);
+    }
+
+    protected Object parseValue(String value) {
+        return Integer.parseInt(value);
     }
 }
--- src/main/java/com/geektime/tdd/cli/StringOptionParser.java
+++ src/main/java/com/geektime/tdd/cli/StringOptionParser.java
@@ -1,11 +1,8 @@
 package com.geektime.tdd.cli;
 
-import java.util.List;
-
-class StringOptionParser implements OptionParser {
+class StringOptionParser extends IntOptionParser {
     @Override
-    public Object parse(List<String> arguments, Option option) {
-        int index = arguments.indexOf(String.format("-%s", option.value()));
-        return arguments.get(index + 1);
+    protected Object parseValue(String value) {
+        return String.valueOf(value);
     }
 }
