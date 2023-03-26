8782878d4669f997f13374d4db9f3ebd9272026a
Hongkui Zhang <hongkui.zhang@thoughtworks.com>
2023-03-26 16:15:31 +0800

    refactor: extract value parsed from optional && add judgement logic struc to allow easier expanding

--- src/main/java/com/geektime/tdd/cli/Args.java
+++ src/main/java/com/geektime/tdd/cli/Args.java
@@ -7,12 +7,19 @@ import java.util.List;
 
 public class Args {
     public static <T> T parse(Class<T> optionType, String... args) {
-        Constructor<?> constructor = optionType.getDeclaredConstructors()[0];
-        Parameter parameter = constructor.getParameters()[0];
-        Optional option = parameter.getAnnotation(Optional.class);
-        List<String> arguments = Arrays.asList(args);
         try {
-            return (T) constructor.newInstance(arguments.contains(String.format("-%s", option.value())));
+            Constructor<?> constructor = optionType.getDeclaredConstructors()[0];
+            Parameter parameter = constructor.getParameters()[0];
+            Optional option = parameter.getAnnotation(Optional.class);
+            List<String> arguments = Arrays.asList(args);
+
+            Boolean value = null;
+
+            if (parameter.getType() == boolean.class) {
+                value = arguments.contains(String.format("-%s", option.value()));
+            }
+
+            return (T) constructor.newInstance(value);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
