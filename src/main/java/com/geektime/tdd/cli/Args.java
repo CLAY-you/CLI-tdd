493566e0670f6649129214ad9c3a7bea5267535a
Hongkui Zhang <hongkui.zhang@thoughtworks.com>
2023-03-26 18:11:53 +0800

    refactor: extract duplicated parameterType declaration && extract get option parser method

--- src/main/java/com/geektime/tdd/cli/Args.java
+++ src/main/java/com/geektime/tdd/cli/Args.java
@@ -25,20 +25,28 @@ public class Args {
 
     private static Object parseOption(List<String> arguments, Parameter parameter) {
         Option option = parameter.getAnnotation(Option.class);
-        OptionParser parser = null;
 
-        if (parameter.getType() == boolean.class) {
+        Class<?> parameterType = parameter.getType();
+
+        OptionParser parser = getOptionParser(parameterType);
+
+        return parser.parse(arguments, option);
+    }
+
+    private static OptionParser getOptionParser(Class<?> parameterType) {
+        OptionParser parser = null;
+        if (parameterType == boolean.class) {
             parser = new BooleanOptionParser();
         }
 
-        if (parameter.getType() == int.class) {
+        if (parameterType == int.class) {
             parser = new IntOptionParser();
         }
 
-        if (parameter.getType() == String.class) {
+        if (parameterType == String.class) {
             parser = new StringOptionParser();
         }
-        return parser.parse(arguments, option);
+        return parser;
     }
 
     static class BooleanOptionParser implements OptionParser {
